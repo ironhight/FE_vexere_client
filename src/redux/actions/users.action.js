0c2f51b2050389004557cc80c73f51bb92c35cb1
ironhight <mrnguyenhoainam2468@gmail.com>
2020-08-01 20:43:40 +0700

    add redux for user

---
+++ src/redux/actions/users.action.js
@@ -0,0 +1,86 @@
+import api from "../../api";
+import swal from "sweetalert2";
+import * as types from "../constants/actionTypes";
+
+export const getProfileAdmin = () => (dispatch) => {
+  return api
+    .get(`/users/me`)
+    .then((res) => {
+      console.log("getProfileAdmin -> res", res);
+
+      dispatch({
+        type: types.GET_PROFILE_ADMIN,
+        payload: res.data,
+      });
+    })
+    .catch(console.log());
+};
+
+export const updateAdmin = (userID, value, callbackThen, callbackCatch) => (dispatch) => {
+  return api
+    .patch(`users/me`, value)
+    .then((res) => {
+      dispatch({
+        type: types.UPDATE_PROFILE_ADMIN,
+        payload: res.data,
+      });
+      Promise.resolve(res.data);
+      swal({
+        text: "Update successfully",
+        icon: "success",
+        buttons: false,
+        timer: 1500,
+      });
+      callbackThen();
+    })
+    .catch((err) => callbackCatch(err));
+};
+
+export const updatePassword = (userID, value, callbackThen, callbackReset, callbackCatch) => (
+  dispatch
+) => {
+  return api
+    .put(`users/change-password/me`, value)
+    .then((res) => {
+      dispatch({ type: types.UPDATE_PASSWORD_ADMIN, payload: res.data });
+      Promise.resolve(res.data);
+      swal({
+        text: "Update successfully",
+        icon: "success",
+        buttons: false,
+        timer: 1500,
+      });
+      callbackThen();
+      callbackReset();
+    })
+    .catch((err) => callbackCatch(err));
+};
+
+export const updateAvatar = (value, config, callbackThen) => (dispatch) => {
+  return api
+    .post(`users/me/avatar`, value, config)
+    .then((res) => {
+      dispatch({ type: types.UPDATE_AVATAR, payload: res.data });
+      swal({
+        text: "Update successfully",
+        icon: "success",
+        buttons: false,
+        timer: 1500,
+      });
+      callbackThen();
+    })
+    .catch((err) => {
+      console.log(err.response);
+    });
+};
+
+export const getAvatar = (id) => (dispatch) => {
+  return api
+    .get(`users/me/${id}/avatar`)
+    .then((res) => {
+      dispatch({ type: types.GET_AVATAR, payload: res.data });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,13 +4,15 @@ import stations from "./stations";
 import trips from "./trips";
 import countTrips from "./countTrips";
 import seatsReducers from "./seats.reducer";
+import usersReducer from "./users.reducer";
 
 const rootReducers = combineReducers({
   Authenticate,
   stations,
   trips,
   countTrips,
-  seatsReducers
+  seatsReducers,
+  usersReducer,
 });
 
 export default rootReducers;
